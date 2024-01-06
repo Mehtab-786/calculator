066d5eb1d7a47128c16cd49d2559a16fb6f5c809
Mehtab <150262610+Mehtab-786@users.noreply.github.com>
2024-01-06 11:49:37 +0530

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,29 +1,46 @@
-def add(n1,n2):
+def add(n1, n2):
     return n1 + n2
 
 
-def subtract(n1,n2):
+def subtract(n1, n2):
     return n1 - n2
 
 
-def divide(n1,n2):
-    return n1/n2
+def divide(n1, n2):
+    return n1 / n2
 
 
-def multiply(n1,n2):
+def multiply(n1, n2):
     return n1 * n2
 
 
-operations = {"+":add ,"-":subtract ,"/":divide ,"*":multiply}
+operations = {"+": add, "-": subtract, "/": divide, "*": multiply}
 
 num1 = int(input("enter your first number :"))
 num2 = int(input("enter your second number :"))
 
-for i in operations: # for showing the options to user
+for i in operations:  # for showing the options to user
     print(i)
 
 symbol = input("Pick an operation from the line above : ")
 calculation = operations[symbol]
-answer = calculation(num1,num2)
+answer = calculation(num1, num2)
 
-print(f"{num1} {symbol} {num2} = {answer}") # used f-string
+print(f"{num1} {symbol} {num2} = {answer}")  # used f-string
+
+calculate_further = False
+
+while not calculate_further:
+    if input("Do you want to calculate further : ") == "yes":
+        another_operation = input("pick another operation :")
+        next_number = int(input("enter next number :"))
+        num = answer
+
+        calculate = operations[another_operation]
+        answer = calculate(num, next_number)
+        print(f"{num} {another_operation} {next_number} = {answer}")
+    else:
+        calculate_further = True
+
+
+
